7ab7bf040d98279fb3045f01b4f32b056cc76cd2
Dawon-yoon <dawon3887@gmail.com>
2024-07-10 15:46:02 +0900

    add my joson server url

--- src/components/SubNav.js
+++ src/components/SubNav.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SubNav = ({title}) => {
   return (
     <div className='sub-nav-area'>
-        <h2>{ title}</h2>      
+        <h2>{title}</h2>      
     </div>
   )
 }
